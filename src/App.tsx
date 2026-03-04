e564df9bbab3d928d95103815b2fcc65615cd81f
Gbadamosi Bashir Olamide <gbadamosibashir1738@gmail.com>
2026-03-04 16:48:29 +0100

    style: ensure minimum height for main container in App component

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <WalletProvider>
       <Router>
-        <div className=" bg-neutral-950 flex items-center justify-center p-4">
+        <div className="min-h-screen bg-neutral-950 flex items-center justify-center p-4">
           <div className="w-90 h-150 bg-black text-white border-neutral-900 shadow-2xl">
             <Routes>
               <Route path="/" element={<Welcome />} />
